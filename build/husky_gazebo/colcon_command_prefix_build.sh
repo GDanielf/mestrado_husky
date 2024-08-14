c5ec4e5caea9e7fe929dca56eb7f1a5bb875958e
GDanielf <gudanielf@gmail.com>
2024-08-13 21:03:22 -0300

    Remove repositório Git interno e adiciona conteúdo ao repositório principal

---
+++ build/husky_gazebo/colcon_command_prefix_build.sh
@@ -0,0 +1,3 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/gdaniel/husky_ws/install/husky_description/share/husky_description/package.sh"
+. "/home/gdaniel/husky_ws/install/husky_control/share/husky_control/package.sh"
---
+++ build/husky_simulator/colcon_command_prefix_build.sh
@@ -0,0 +1,4 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/gdaniel/husky_ws/install/husky_description/share/husky_description/package.sh"
+. "/home/gdaniel/husky_ws/install/husky_control/share/husky_control/package.sh"
+. "/home/gdaniel/husky_ws/install/husky_gazebo/share/husky_gazebo/package.sh"
